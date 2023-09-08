5af6f5cc23f7656964e7dd1ba32a033f44c44ba3
Emtkomb <104244366+Emtkomb@users.noreply.github.com>
2023-09-08 07:51:53 +0300

    Add files via upload

---
+++ ToDoWebApi/Controllers/ToDoController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using ToDo.BusinessLayer.Abstract;
+using ToDo.DataAccessLayer.Abstract;
+using ToDo.EntityLayer.Concrete;
+
+namespace ToDoWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly IToDoService _todoservice;
+
+        public ToDoController(IToDoService todoservice)
+        {
+            _todoservice = todoservice;
+        }
+
+        [HttpGet]
+         public IActionResult ToDoList()
+        {
+            var values=_todoservice.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddToDo(todo todoo) 
+        {
+            _todoservice.TInsert(todoo);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteToDo(int id)
+        {
+            var values=_todoservice.TGetByID(id);
+            _todoservice.TDelete(values);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateToDo(todo todoo)
+        {
+            _todoservice.TUpdate(todoo);
+            return Ok();
+        }
+        [HttpGet("(id)")]
+        public IActionResult GetToDo(int id)
+        {
+            var values = _todoservice.TGetByID(id);
+
+            return Ok(values);
+        }
+    }
+}
---
+++ ToDoWebApi/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DtoLayer.Dtos.ToDoDto;
+using ToDo.EntityLayer.Concrete;
+
+namespace ToDoWebApi.Mapping
+{
+    public class AutoMapperConfig:Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<ToDoAddDto, todo>();
+            CreateMap<todo,ToDoAddDto>();
+
+            CreateMap<UpdateToDoDto, todo>().ReverseMap();
+            
+        }
+    }
+}
