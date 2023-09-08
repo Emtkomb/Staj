4e8325d58d931d2166c68a669f7ac234d83590f6
Emtkomb <104244366+Emtkomb@users.noreply.github.com>
2023-09-08 07:45:02 +0300

    Add files via upload

---
+++ DtoLayer/Dtos/ToDoDto/ToDoAddDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoLayer.Dtos.ToDoDto
+{
+    public class ToDoAddDto
+    {
+
+
+        [Required(ErrorMessage = "Lütfen kullanıcı adı giriniz")]
+        public string userName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen ToDo adresinizi giriniz")]
+        public string Todo { get; set; }
+        
+    }
+}
---
+++ DtoLayer/Dtos/ToDoDto/UpdateToDoDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoLayer.Dtos.ToDoDto
+{
+    public class UpdateToDoDto
+    {
+
+        [Required(ErrorMessage = "Lütfen kullanıcı adı giriniz")]
+        public string userName { get; set; }
+        [Required(ErrorMessage = "Lütfen mail adresinizi giriniz")]
+        public string Todo { get; set; }
+    }
+}
