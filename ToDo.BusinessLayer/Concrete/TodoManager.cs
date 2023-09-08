b6cc5b18b7e04bed62f940683fbe9438d0b03348
Emtkomb <104244366+Emtkomb@users.noreply.github.com>
2023-09-08 07:45:43 +0300

    Add files via upload

---
+++ ToDo.BusinessLayer/Concrete/TodoManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.BusinessLayer.Abstract;
+using ToDo.DataAccessLayer.Abstract;
+using ToDo.EntityLayer.Concrete;
+
+namespace ToDo.BusinessLayer.Concrete
+{
+    public class TodoManager : IToDoService
+    {
+        private readonly IToDoDal _tododal;
+
+        public TodoManager(IToDoDal tododal)
+        {
+            _tododal = tododal;
+        }
+
+        public void TDelete(todo t)
+        {
+            _tododal.Delete(t);
+            
+        }
+
+        public todo TGetByID(int id)
+        {
+            return _tododal.GetByID(id);
+        }
+
+        public List<todo> TGetList()
+        {
+            return _tododal.GetList();
+        }
+
+        public void TInsert(todo t)
+        {
+            _tododal.Insert(t);
+        }
+
+        public void TUpdate(todo t)
+        {
+            _tododal.Update(t);
+        }
+    }
+}
