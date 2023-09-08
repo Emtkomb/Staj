1a49c71d6354d8450e90e1834c4bdb6dcf825c08
Emtkomb <104244366+Emtkomb@users.noreply.github.com>
2023-09-08 07:47:02 +0300

    Add files via upload

---
+++ ToDo.EntityLayer/Concrete/todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.EntityLayer.Concrete
+{
+    public class todo
+    {
+        public int Id { get; set; }
+        public string userName { get; set; }
+        public string Todo { get; set; }
+       
+    }
+}
